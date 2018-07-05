6cbaf35c413074e56641c764f3dc40999e036607
Chiran Doshi <chirandoshi@gmail.com>
2018-07-05 16:40:52 +0530

    enable cors

--- index.js
+++ index.js
@@ -14,21 +14,9 @@ app.use(bodyParser.urlencoded({
 
 
 app.use(function(req, res, next) {
-  if (req.method === 'OPTIONS') {
-    console.log('!OPTIONS');
-    var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
-    res.end();
-  } else {
-    next();
-  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // Any get request would be routed to the home page
